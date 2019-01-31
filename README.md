88a300a25a6c02cc39c363f2889e0d547c720a6a
Josh <josh@visionistinc.com>
2019-01-31 18:23:15 -0500

    Initial plotly.js skeleton running with parcel.

---
+++ README.md
@@ -0,0 +1,8 @@
+Initial skeleton of a plotly.js app working w/ Parcel.
+
+To start development:
+
+```
+npm install -g parcel-bundler
+parcel index.html
+```
---
+++ index.js
@@ -0,0 +1,7 @@
+import Plotly from 'plotly.js-dist';
+
+let App = document.getElementById("app");
+Plotly.plot( App, [{
+	x: [1, 2, 3, 4, 5],
+	y: [1, 2, 4, 8, 16] }], {
+	margin: { t: 0 } } );
